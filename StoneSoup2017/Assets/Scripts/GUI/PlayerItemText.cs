0c1b15e4feee32a0420a9ddc669591f71fd239c3
Gordon Laptop <gordon.lee.6170@gmail.com>
2017-12-11 15:10:26 -0500

    new font, item sprite

--- StoneSoup2017/Assets/Scripts/GUI/PlayerItemText.cs
+++ StoneSoup2017/Assets/Scripts/GUI/PlayerItemText.cs
@@ -5,22 +5,48 @@ using UnityEngine.UI;
 
 public class PlayerItemText : MonoBehaviour {
 
-	protected Text _text;
+    [SerializeField] private Sprite[] itemSprites;
+    private Image uiImage;
+
+	//protected Text _text;
 
 	// Use this for initialization
 	void Start () {
-		_text = GetComponent<Text>();
-	}
+        //_text = GetComponent<Text>();
+        uiImage = this.GetComponent<Image>();
+
+    }
 		
 	// Update is called once per frame
 	void Update () {
 		if (Player.instance != null) {
 			if (Player.instance.tileWereHolding != null) {
-				_text.text = string.Format("Item: {0}", Player.instance.tileWereHolding.tileName);
+                //_text.text = string.Format("Item: {0}", Player.instance.tileWereHolding.tileName);
+                uiImage.color = new Color32(255, 255, 255, 255);
+                
+                switch (Player.instance.tileWereHolding.tileName) {
+                    case "Swing Thing":
+                        uiImage.sprite = itemSprites[0];
+                        uiImage.SetNativeSize();
+                        break;
+                    case "thing2throw":
+                        uiImage.sprite = itemSprites[1];
+                        uiImage.SetNativeSize();
+                        break;
+                    case "bow":
+                        uiImage.sprite = itemSprites[2];
+                        uiImage.SetNativeSize();
+                        break;
+                    case "throwUp":
+                        uiImage.sprite = itemSprites[3];
+                        uiImage.SetNativeSize();
+                        break;
+                }
 			}
 			else {
-				_text.text = "Item: None";
-			}
+                //_text.text = "Item: None";
+                uiImage.color = new Color32(255, 255, 255, 0);
+            }
 		}
 	}
 }
